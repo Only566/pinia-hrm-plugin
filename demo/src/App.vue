06c17ce8238fd7997f08f9db965298111e07bc8e
winnliu <@waw981109@>
2025-05-26 22:58:18 +0800

    feat(demo): test multi store

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
-import { useCounterStore } from './counter'
+import {useCounterStore, useCounterStore2} from './counter';
 import { storeToRefs } from 'pinia'
 
 const counterStore = useCounterStore()
+const counterStore2 = useCounterStore2()
 const { count } = storeToRefs(counterStore)
+const { count: count2 } = storeToRefs(counterStore2)
 </script>
 
 <template>
-  <h1>Hello {{count}}</h1>
+  <h1>counter1 {{count}}</h1>
   <button>
     <span @click="counterStore.increment">Increment</span>
   </button>
   <button>
     <span @click="counterStore.throwError">Throw Error</span>
+  </button>
+  <h1>counter2 {{count2}}</h1>
+  <button>
+    <span @click="counterStore2.increment">Increment</span>
+  </button>
+  <button>
+    <span @click="counterStore2.throwError">Throw Error</span>
   </button>
 </template>
--- demo/src/counter.ts
+++ demo/src/counter.ts
@@ -1,14 +1,28 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const increment = () => {
-    count.value += 1
-  }
-  const throwError = () => {
-    throw new Error('Error from store')
-  }
+export const useCounterStore =
+  defineStore('counter', () => {
+    const count = ref(0)
+    const increment = () => {
+      count.value += 2
+    }
+    const throwError = () => {
+      throw new Error('Error from store')
+    }
 
-  return { count, increment, throwError }
-})
+    return { count, increment, throwError }
+  })
+
+export const useCounterStore2 =
+  defineStore('counter2', () => {
+    const count = ref(0)
+    const increment = () => {
+      count.value += 1
+    }
+    const throwError = () => {
+      throw new Error('Error from store')
+    }
+
+    return { count, increment, throwError }
+  })
